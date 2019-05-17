041cd895ad5416415f4d96e1d279b443aa3aac02
Aditya Singgih <adityasinggih@msn.com>
2019-05-17 12:07:20 +1000

    edited data type on model due to copy pasting error

--- TodoAPI/Models/TodoItem.cs
+++ TodoAPI/Models/TodoItem.cs
@@ -4,9 +4,9 @@
 
         // ID will be the unique key inside the DB
         public long Id { get; set; }
-        public long Name{ get; set; }
+        public string Name{ get; set; }
         // Flag to check whether task has been completed
-        public long IsComplete { get; set; }
+        public bool IsComplete { get; set; }
 
     }
 }
