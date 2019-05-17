671099b350dc8b822a6d0b289dbf05f10ab88710
Aditya Singgih <adityasinggih@msn.com>
2019-05-17 11:45:14 +1000

    Added TodoItem Model and TodoAPI db context

---
+++ TodoAPI/Models/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoAPI.Models {
+
+    // this class is inheriting from DbContext
+    public class TodoContext : DbContext {
+
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) {
+        
+        }
+
+        // Just like Java this is <DataType>
+        public DbSet<TodoItem> TodoItems { get; set;}
+    }
+}
---
+++ TodoAPI/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoAPI.Models {
+
+    public class TodoItem {
+
+        // ID will be the unique key inside the DB
+        public long Id { get; set; }
+        public long Name{ get; set; }
+        // Flag to check whether task has been completed
+        public long IsComplete { get; set; }
+
+    }
+}
