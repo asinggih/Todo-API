d995fd5a3b90685dbb6abcf392583a19e99205f3
Aditya Singgih <adityasinggih@msn.com>
2019-05-17 11:47:00 +1000

    added the Todo DbContext into our DI container

--- TodoAPI/Startup.cs
+++ TodoAPI/Startup.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
 
-namespace TodoAPI
-{
+using TodoAPI.Models;
+
+namespace TodoAPI {
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -25,6 +20,12 @@ namespace TodoAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Adding the db context to this method, which is our 
+            // dependency injection container
+            
+            services.AddDbContext<TodoContext>(opt =>
+               opt.UseInMemoryDatabase("TodoList")
+            );
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
